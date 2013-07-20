999694962ef366d67f3900f9d3c28cd1530595b8
Ubuntu <tgoerner@gmail.com>
2013-07-20 23:30:25 +0000

    working on integrating requestHandlers

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-var http = require("http"); var url = require("url"); function start() {
+var http = require("http"); 
+var url = require("url"); 
+var router = require("./router");
+
+function start() {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received");
+
+		router.route(handle, pathname);
+
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World");
 		response.end();

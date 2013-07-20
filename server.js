1b549b3a615be6332a50b2db3a16ac31f669b029
Ubuntu <tgoerner@gmail.com>
2013-07-20 22:46:25 +0000

    first running server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require("http"); var url = require("url"); function start() {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
-		console.log("Request for " + pathname + " received";
+		console.log("Request for " + pathname + " received");
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World");
 		response.end();

24020aa76359391b5c8157f747eb3afa5f137614
Ubuntu <tgoerner@gmail.com>
2013-07-21 00:34:57 +0000

    Working blocking object returned to response object! Flow of applications reversed, now response passed from server > router > requestHandler and processed inside requestHandler

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,18 +1,20 @@
+var exec = require("child_process").exec;
 
-function start() {
+function start(response) {
 	console.log("Request handler 'start' was called.");
 	
-	function sleep(milliSeconds) {
-		var startTime = new Date().getTime();
-		while (new Date().getTime() < startTime + milliSeconds);
-	}
-	sleep(10000);
-	return "Hello start";
+	exec("ls -lah", function (error, stdout, stderr) {
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write(stdout);
+		response.end();
+	});
 }
 
-function upload() {
+function upload(response) {
 	console.log("Request handler 'upload' was called.");
-	return "Hello Upload";
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello Upload");
+	response.end();
 }
 
 exports.start = start;
